4f55779286b2ec624a31a263f28423809f2b59ff
이준희 <junh0328@naver.com>
2020-09-30 16:41:44 +0900

    7. Navigation bar 만들기, media screen 기능을 사용해서 반응형으로 움직일 수 있는 버튼 생성

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Home from './routes/Home';
 import './App.css';
 import { HashRouter, Route } from 'react-router-dom';
 import About from './routes/About';
+import Navigation from './components/Navigation'
 
 function App(){
   return (
     <HashRouter>
+      <Navigation/>
       <Route path="/" exact={true} component={Home}/>
       <Route path="/about" component={About}/>
     </HashRouter>
