8054aac2583eb5777421009fa810a804f24220f5
이준희 <junh0328@naver.com>
2020-09-30 16:55:40 +0900

    Updates

---
+++ precache-manifest.968bd42098aeb037e2b094258a983261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65db0e2a5d13a7879060a116e1dfae21",
+    "url": "/movie_app_review/index.html"
+  },
+  {
+    "revision": "add6c1fe419214fcfaf0",
+    "url": "/movie_app_review/static/css/main.bfb70da5.chunk.css"
+  },
+  {
+    "revision": "2ce93c02b2dcdf692fdf",
+    "url": "/movie_app_review/static/js/2.b2709b37.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_review/static/js/2.b2709b37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add6c1fe419214fcfaf0",
+    "url": "/movie_app_review/static/js/main.aaf90a2f.chunk.js"
+  },
+  {
+    "revision": "5227255dfec3a8a5b0f2",
+    "url": "/movie_app_review/static/js/runtime-main.ebb01808.js"
+  }
+]);
